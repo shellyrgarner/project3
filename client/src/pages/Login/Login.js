be0a68c1ce4a7df92931dae3d4f2157003ec819b
shellyrgarner <sgrenea@gmail.com>
2018-10-13 11:21:46 -0400

    Refactor && Fix

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./login.css";
+
+
+
+class Login extends Component {
+
+    render()
+    {
+        return(
+            <div>
+                <h1>Login coming eventually</h1>
+            </div>
+        )
+    }
+    
+
+}
+
+export default Login;
---
+++ routes/api/conferences.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const confController = require("../../controllers/confController");
+
+//matches with "/api/conferences"
+//router.route("/")
+//.get(confController.findAll)
+ //.post(confController.create);
+
+//matches with "/api/conferences/schedule"
+//router
+//.route("/schedule")
+//.get(confController.findById);
+// .put(confController.update)
+// .delete(confController.remove);
+
+module.exports = router;
