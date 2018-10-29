f6ba986aaf72da51465786f59fa5417966228aa4
shellyrgarner <sgrenea@gmail.com>
2018-10-29 18:44:45 -0400

    trying to merge sam's update couldnt do in github

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 //import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Conferences from "./pages/Conferences";
-import Schedule from "./pages/Schedule";
-import TravelTips from "./pages/TravelTips";
+// import Schedule from "./pages/Schedule";
+// import TravelTips from "./pages/TravelTips";
 import Nav from "./components/Nav";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
@@ -23,10 +23,10 @@ const App = () => (
         <Route exact path="/" component={Welcome} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/conferences" component={Conferences} />
-        <Route exact path="/conferences/schedule" component={Schedule} />
+        {/* <Route exact path="/conferences/schedule" component={Schedule} /> */}
         <Route exact path="/conferences/:id" component={Details} />
         {/* <Route exact path="/something" component={NoMatch} /> */}
-        <Route exact path="/traveltips" component={TravelTips} />
+        {/* <Route exact path="/traveltips" component={TravelTips} /> */}
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/settings" component={Settings} />
         <Route component={NotFound} />
--- client/src/pages/Details/Details.js
+++ client/src/pages/Details/Details.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
@@ -22,39 +22,44 @@ class Details extends Component {
 
     // handleFormSubmit = event => {
     //     event.preventDefault();
-        // if (this.state.event && this.state.location) {
-        //   API.postConference({
-        //     event: this.state.event,
-        //     beginDate: this.state.beginDate,
-        //     endDate: this.state.endDate,
-        //     venue: this.state.venue,
-        //     location: this.state.location,
-        //     info: this.state.info
-        //   })
-        //     .then(res => this.loadConferences())
-            // .catch(err => console.log("handleformsubmit ERROR: " + err));
-        // }
-      
+    // if (this.state.event && this.state.location) {
+    //   API.postConference({
+    //     event: this.state.event,
+    //     beginDate: this.state.beginDate,
+    //     endDate: this.state.endDate,
+    //     venue: this.state.venue,
+    //     location: this.state.location,
+    //     info: this.state.info
+    //   })
+    //     .then(res => this.loadConferences())
+    // .catch(err => console.log("handleformsubmit ERROR: " + err));
+    // }
+
 
     render() {
         return (
             <Container fluid>
                 <Row>
                     <Col size="md-12">
-                    <Hero imgClass="hero-img5">
-
-                    <h2>Schedule</h2>
-                    <br></br>
-       
+                        <Hero imgClass="hero-img5">
+                            <h1>Conference Details</h1>
+                            {/* <h3>{this.state.conference.event}</h3>
+                            <p>{this.state.conference.beginDate}-{this.state.conference.endDate}</p> */}
+                            <br></br>
+                            {/* <Jumbotron>
                             <h3>{this.state.conference.event}</h3>
                             <p>{this.state.conference.beginDate}-{this.state.conference.endDate}</p>
+                        </Jumbotron> */}
                         </Hero>
                     </Col>
                 </Row>
                 <Row>
                     <Col size="md-10 md-offset-1">
                         <article>
-                            <h1>Conference Details</h1>
+                        <br></br>
+                            <h3>{this.state.conference.event}</h3>
+                            <h3>{this.state.conference.beginDate}-{this.state.conference.endDate}</h3>
+                            <br></br>
                             <p>Venue: {this.state.conference.venue}</p>
                             <p>Location: {this.state.conference.location}</p>
                             <p>Details: {this.state.conference.info}</p>
@@ -68,10 +73,10 @@ class Details extends Component {
                 </Row>
                 <Row>
                     {/* <button>Register</button> */}
-                    <Col size="md-12"> 
-                    {/* onClick={this.handleFormSubmit} */}
+                    <Col size="md-12">
+                        {/* onClick={this.handleFormSubmit} */}
                         <RegisterBtn>Register</RegisterBtn>
-                     </Col>
+                    </Col>
                 </Row>
                 <Footer />
             </Container>
